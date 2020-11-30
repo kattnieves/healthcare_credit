91d140e4bee158a27df268c0408e129c3a22a003
Arkit Pawar <aap247@rutgers.edu>
2020-11-30 17:59:20 -0500

    Version2.0

--- fl.py
+++ fl.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f0799c2163525275f109b3c7893ac4fe'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
@@ -24,6 +24,16 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
+    address = StringField('Address', validators = [DataRequired()])
+    address2 = StringField('Address 2')
+    city = StringField('City', validators = [DataRequired()])
+    submit = SubmitField('Sign Up')
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -33,6 +43,13 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.is_patient}')"
+
+class LocatorForm(FlaskForm):
+    practice_name = StringField('Search by Practice Name')
+    speciality = SelectField('Speciality', choices = ['Choose a Category', 'Anesthesiology', 'Chiropractor', 'Orthodentist', 'Dermatology', 'Neurology', 'Pathology', 'Psychiatry', 'Surgery', 'Urology', 'Dermatology', 'Chiropractor', 'Dental', 'Anesthesiology', 'Cardiology', 'Gastroenterology', 'Psychiatry', 'Surgery', 'Radiology', 'Psychiatry', 'Urology', 'Gastroenterology', 'Hepatology', 'Dental', 'Orthodentist', 'Surgery', 'Neurology', 'Cardiology', 'Immunology', 'Psychiatry'])
+    radius = SelectField('Radius', choices = [str(i) + ' miles' for i in [5, 10, 20, 50, 75]])
+    city_zip = StringField('City or Zip')
+    submit = SubmitField('Search')
 
 class Affiliate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -56,7 +73,55 @@ def match_rows(speciality = None, city = None, state = None, zip_code = None):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    form = LocatorForm()
+    return render_template('index.html', underline = 'home', form = form)
+
+@app.route("/about")
+def about():
+    return render_template('aboutus.html', underline = 'about')
+
+@app.route("/locator")
+def locator():
+    print(request.args)
+    print('In locator')
+    form = LocatorForm(request.args, csrf_enabled=False)
+    results = None
+    if form.validate():
+        print('In validate submit')
+
+        if not form.practice_name.data:
+            form.practice_name.data = ''
+        if not form.city_zip.data:
+            form.city_zip.data = ''
+        if form.speciality.data == 'Choose a Category':
+            form.speciality.data = ''
+        is_zip = form.city_zip.data.isdigit()
+        if is_zip:
+            results = Affiliate.query.filter(Affiliate.name.ilike(f'%{ form.practice_name.data.strip() }%')).filter(Affiliate.speciality.ilike(f'%{ form.speciality.data.strip() }%')).filter(Affiliate.zip_code == form.city_zip.data.strip())
+        else:
+            results = Affiliate.query.filter(Affiliate.name.ilike(f'%{ form.practice_name.data.strip() }%')).filter(Affiliate.speciality.ilike(f'%{ form.speciality.data.strip() }%')).filter(Affiliate.city.ilike(f'%{ form.city_zip.data.strip() }%'))
+        sort_name = request.args.get('sort_name')
+        if sort_name == 'A':
+            results = results.order_by(Affiliate.name)
+        elif sort_name == 'D':
+            results = results.order_by(Affiliate.name.desc())
+        results = results.paginate(per_page = 100)
+    return render_template('locator.html', underline = 'locator', form = form, results = results)
+
+@app.route("/doctors/", defaults={'page_num': 1})
+@app.route("/doctors/<int:page_num>/")
+def doctors(page_num):
+    doctors = Affiliate.query.paginate(per_page = 10, page = page_num)
+    print(page_num)
+    return render_template('doctors.html', underline = 'doctors', doctors = doctors)
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', underline = 'contact')
+
+@app.route("/register")
+def signup():
+    return render_template('signup.html')
 
 @app.route("/all")
 def all():
@@ -74,9 +139,11 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email = form.email.data).first()
         if user and user.password == form.password.data:
-            return f"<h1> { user.username } has logged in"
+            flash(f'{ user.username } has logged in', 'success')
+            return redirect(url_for('home'))
+            #return f"<h1> { user.username } has logged in"
         else:
-            return redirect("https://example.com/fail")
+            return "<h1>Login failed</h1>"
     else:
         return render_template('login.html', form = form)
 
